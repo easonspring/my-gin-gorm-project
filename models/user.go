0862c196a046819071c3168594678fc9db0a0b60
yixian.wang <2483004368@qq.com>
2025-03-28 19:49:12 +0800

    add query and create api

--- models/user.go
+++ models/user.go
@@ -3,9 +3,10 @@ package models
 import (
 	"gorm.io/gorm"
 ) // User 用户模型定义
+
 type User struct {
 	gorm.Model
-	Name  string `gorm:"size:255"`
-	Email string `gorm:"uniqueIndex"`
-	Age   int
+	Name  string `gorm:"size:255;comment:用户名"`
+	Email string `gorm:"type:varchar(100);uniqueIndex;comment:邮箱"`
+	Age   int    `gorm:"default:18;comment:年龄"`
 }
